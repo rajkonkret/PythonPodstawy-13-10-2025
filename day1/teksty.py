da88ec6d4f3e46a97d630c96853768048e64ca45
rajkonkret <rajkonkret660@gmail.com>
2025-10-13 11:11:02 +0200

    [1 5] update teksty.py

--- day1/teksty.py
+++ day1/teksty.py
@@ -28,3 +28,14 @@ encoded_s = tekst.encode('utf-8')
 print(encoded_s)  # b'Witaj \xc5\x9awiecie'
 print(type(encoded_s))  # <class 'bytes'>, typ bajtowy
 print(encoded_s.decode("utf-8"))  # Witaj Świecie
+
+print(tekst)
+# Witaj Świecie
+# 0123456789....
+
+print(tekst[4])  # "j" indeks 4
+
+print(tekst.count("i"))  # 3 występuje 3 razy
+print(tekst.count("j", 0, 4))  # wystepuje 0 razy, indeksy 0123, z prawej zbiór otwarty
+
+
