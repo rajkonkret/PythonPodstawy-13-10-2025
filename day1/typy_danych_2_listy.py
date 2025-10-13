0b33c88af5367eab388b2f9a309bd0c6fe366d56
rajkonkret <rajkonkret660@gmail.com>
2025-10-13 12:51:10 +0200

    [1 17] update typy_danych_2_listy.py

--- day1/typy_danych_2_listy.py
+++ day1/typy_danych_2_listy.py
@@ -36,3 +36,33 @@ print(lista[-1])  # Aga, ostatni elemnt z listy
 #     -6       -5       -4        -3        -2     -1
 
 print(lista[-2])  # Anna
+
+# fragment z listy - slicowanie
+print(lista[0:3])  # 012, ['Radek', 'Tomek', 'Zenek']
+print(lista[:3])  # ['Radek', 'Tomek', 'Zenek']
+print(lista[2:])  # ['Zenek', 'Oliwier', 'Anna', 'Aga'] - z ostatnim włącznie
+print(lista[2:5])  # ['Zenek', 'Oliwier', 'Anna']
+
+print(lista[2:15])  # ['Zenek', 'Oliwier', 'Anna', 'Aga']
+print(lista[20:234])  # [] - pusta lista
+
+print(lista[:])  # ['Radek', 'Tomek', 'Zenek', 'Oliwier', 'Anna', 'Aga']
+
+print(lista[2:2])  # []
+print(lista[2:3])  # ['Zenek']
+
+print(lista[::2])  # [start:stop:krok], ['Radek', 'Zenek', 'Anna'], co drugi
+
+# ['Radek', 'Tomek', 'Zenek', 'Oliwier', 'Anna', 'Aga']
+#     0        1        2         3         4      5
+#     -6       -5       -4        -3        -2     -1
+print(lista[-2:0])  # [], [4:0]
+print(lista[0:-2])  # [0:4] # ['Radek', 'Tomek', 'Zenek', 'Oliwier']
+
+lista_15 = list(range(15))  # int od 0 do 14
+print(lista_15)  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+print(lista_15[::2])  # [0, 2, 4, 6, 8, 10, 12, 14]
+
+print(lista_15[::-1])  # [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+print(lista_15[::-3])  # [14, 11, 8, 5, 2]
+
