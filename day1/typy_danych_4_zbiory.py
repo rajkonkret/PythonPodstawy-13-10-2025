fe99ffb8a37b86d825aacab731f31bb303a31899
rajkonkret <rajkonkret660@gmail.com>
2025-10-13 14:20:46 +0200

    [1 26] add typy_danych_4_zbiory.py

---
+++ day1/typy_danych_4_zbiory.py
@@ -0,0 +1,49 @@
+# zbiór (set) - przechowują unikalne wartości
+# nie zachowują kolejności przy dodawania elementów
+# nie mają indexu
+
+lista = [44, 55, 66, 777, 33, 22, 11, 33, 11]
+zbior = set(lista)
+print(type(zbior))  # <class 'set'>
+print(zbior)  # {33, 66, 777, 11, 44, 22, 55}
+
+# pusty zbiór
+zb2 = set()  # pusty zbiór tworzymy za pomoca słówka set()
+print(zb2)  # set() - pusty zbiór
+print(type(zb2))  # <class 'set'>
+
+# dodanie elementu do zbioru
+zbior.add(33)
+zbior.add(33)
+zbior.add(33)
+zbior.add(18)
+zbior.add(18)
+zbior.add(18)
+zbior.add(33)
+zbior.add(24)
+zbior.add(32)
+zbior.add(17)
+print(zbior)
+# {32, 33, 66, 777, 11, 44, 17, 18, 22, 55, 24}
+
+# usunięcie elemntu ze zbioru
+zbior.remove(55)
+print(zbior)  # {32, 33, 66, 777, 11, 44, 17, 18, 22, 24}
+
+# pop() - usunięcie pierwszego elementu
+print(zbior.pop())  # 32
+print(zbior)  # {33, 66, 777, 11, 44, 17, 18, 22, 24}
+
+print(zbior.pop())  # 33
+print(zbior)  # {66, 777, 11, 44, 17, 18, 22, 24}
+
+zmienna = zbior.pop()
+print("Usunięty:", zmienna)  # Usunięty: 66
+
+# operacje na zbiorach
+zbior_2 = {667, 11, 44, 12.34, 18, 52, 667, 62}
+print(type(zbior_2))  # <class 'set'>
+print(zbior_2)  # {18, 667, 52, 11, 44, 12.34, 62}
+
+# suma zbiorów - zwraca nowy zbiór
+print(zbior | zbior_2)  # {777, 11, 44, 12.34, 17, 18, 52, 22, 24, 667, 62}
