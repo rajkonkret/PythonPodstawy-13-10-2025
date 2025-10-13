f3b5c59523e955f7f114619ee9aa5b48bf40f4dd
rajkonkret <rajkonkret660@gmail.com>
2025-10-13 09:59:52 +0200

    [1 2] update pierwszy.py

--- day1/pierwszy.py
+++ day1/pierwszy.py
@@ -2,3 +2,16 @@
 # ctrl alt l - formatowanie kodu
 
 print()  # wypisz/wydrukuj
+print("Radek")
+print('Radek')
+# Radek
+# Radek
+
+# ctrl / - komentarz
+# print('Radek") - linijka z błędem
+#   File "C:\Users\Szkolenie\PycharmProjects\PythonPodstawy-13-10-2025\day1\pierwszy.py", line 9
+#     print('Radek")
+#           ^
+# SyntaxError: unterminated string literal (detected at line 9)
+#
+# Process finished with exit code 1 - program z błędem
