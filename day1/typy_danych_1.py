ee183e5427a1193d70c61ae9908ff4fc86a5111f
rajkonkret <rajkonkret660@gmail.com>
2025-10-13 12:21:08 +0200

    [1 12] update typy_danych_1.py

--- day1/typy_danych_1.py
+++ day1/typy_danych_1.py
@@ -22,3 +22,46 @@ print(len(str(wiek ** rok)))  # 3386 długość
 
 print(54 - 5 * 4 / 1 + 4 / 2)  # 36.0
 print(54 - (5 * 4 / 1 + 4) / 2)  # 42.0
+
+# float
+# bład zaokrąglenia
+print(0.2 + 0.8)  # 1.0
+print(0.2 + 0.7)  # 0.8999999999999999
+# For example, in a floating-point arithmetic with five base-ten digits,
+# the sum 12.345 + 1.0001 = 13.3451 might be rounded to 13.345.
+# decimal() - obejscie problemu zaokrąglenia
+
+# typ logiczny
+# prawda, fałsz
+# True, False
+# 1, 0
+czy_znasz_pythona = True
+print(czy_znasz_pythona)  # True
+print(type(czy_znasz_pythona))  # <class 'bool'>, typ logiczny, boolean
+
+print(int(True))  # 1
+print(int(False))  # 0
+
+# bool() - rzutowanie na typ logiczny
+print(bool(1))  # True
+print(bool(0))  # False
+
+print(bool(100))  # True
+print(bool(-100))  # True
+print(bool("Radek"))  # True
+
+print(bool(""))  # False
+print(bool(None))  # False, nic, nie wiem, stan nieokreślony, odpowiednik null
+
+# and - i
+print(True and False)  # False
+print(True and True)  # True
+
+# or - lub
+print(True or False)  # True
+print(True or True)  # True
+print(False or False)  # False
+
+# not - negacja
+print(not True)  # False
+print(not False)  # True
