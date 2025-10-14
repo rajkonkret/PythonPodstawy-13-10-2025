7fa70215847f7f2594edf2157d0c5bd81bf776c3
rajkonkret <rajkonkret660@gmail.com>
2025-10-14 14:10:13 +0200

    [2 33]add pliki_zad1.py

---
+++ day2/pliki_zad1.py
@@ -0,0 +1,45 @@
+# działania z plikami
+# context managerów
+# with - context manager w pythonie
+
+# ========= ===============================================================
+#     Character Meaning
+#     --------- ---------------------------------------------------------------
+#     'r'       open for reading (default)
+#     'w'       open for writing, truncating the file first
+#     'x'       create a new file and open it for writing
+#     'a'       open for writing, appending to the end of the file if it exists
+#     'b'       binary mode
+#     't'       text mode (default)
+#     '+'       open a disk file for updating (reading and writing)
+#     ========= ===============================================================
+with open('test.log', "w", encoding="utf-8") as f:  # filehandler
+    f.write("Powitanie\n")
+    f.write("Kolejne\n")
+    f.write("Jeszcze jedno\n")
+
+# FileExistsError: [Errno 17] File exists: 'test.log'
+# x - tworzy nowy plik
+# # jesli istnieje będzie błąd: FileExistsError
+# with open('test.log', 'x', encoding="utf-8") as file:
+#     file.write("Jestem X")
+
+# w - nadpisze plik jeśli juz istnieje
+with open('test.log', "w", encoding="utf-8") as f:
+    f.write("Nowe dane\n")
+
+with open('test.log', "a", encoding="utf-8") as f:
+    f.write("Dopisane\n")
+    f.write("Kolejne\n")
+    f.write("Jeszcze jedno\n")
+    f.write("Dośpisane\n")
+
+with open('test.log', "r", encoding="utf-8") as fh:
+    lines = fh.read()
+
+print(lines)
+# Nowe dane
+# Dopisane
+# Kolejne
+# Jeszcze jedno
+# Dopisane
