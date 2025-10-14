6dcd36dd50794a04a69317b77fb8cb4688ab5bf6
rajkonkret <rajkonkret660@gmail.com>
2025-10-14 12:52:45 +0200

    [2 28] update petle_zad3.py

--- day2/petle_zad3.py
+++ day2/petle_zad3.py
@@ -60,3 +60,13 @@ print(lista_int)  # [1, 2, 3, 4, 5]
 # Podaj liczbę5
 # Podaj liczbęa
 # ['1', '2', '3', '4', '5']
+my_list = [1, 5, 2, 3, 5, 4, 5, 6, 5]
+while 5 in my_list:
+    my_list.remove(5)
+print(my_list)  # [1, 2, 3, 4, 6], zachowuje kolejność
+
+my_list = [1, 5, 2, 3, 5, 4, 5, 6, 5]
+print(dict.fromkeys(my_list))
+# {1: None, 5: None, 2: None, 3: None, 4: None, 6: None}
+unique_numbers = list(dict.fromkeys(my_list))
+print(unique_numbers)  # [1, 5, 2, 3, 4, 6]
