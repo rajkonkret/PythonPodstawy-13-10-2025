8dd05d9591c5a1dce049dfaade4743044efb5f68
rajkonkret <rajkonkret660@gmail.com>
2025-10-15 14:22:18 +0200

    [2 25] addd  arkusz1.py

--- day3/arkusz2.py
+++ day3/arkusz2.py
@@ -24,3 +24,15 @@ print(data[data['Amount'] > 45000])
 # 0 2018-05-12   Sila Ahmed   60000
 # 1 2019-12-06  Mir Hossain   50000
 data.to_excel("nowy_excel.xlsx")
+
+data.info()
+# <class 'pandas.core.frame.DataFrame'>
+# RangeIndex: 4 entries, 0 to 3
+# Data columns (total 3 columns):
+#  #   Column        Non-Null Count  Dtype
+# ---  ------        --------------  -----
+#  0   Sales Date    4 non-null      datetime64[ns]
+#  1   Sales Person  4 non-null      object
+#  2   Amount        4 non-null      int64
+# dtypes: datetime64[ns](1), int64(1), object(1)
+# memory usage: 228.0+ bytes
