9207c47eb5c3c1535ad25c99ebb304e7d7ee89e8
rajkonkret <rajkonkret660@gmail.com>
2025-10-15 12:09:52 +0200

    [2 14] update kl2.py

--- day3/kl2.py
+++ day3/kl2.py
@@ -18,6 +18,12 @@ class Human:
     def powitanie(self):
         print(f"Nazywam się: {self.imie}")
 
+    def ruszaj(self):
+        if self.plec == "m":
+            print("Ruszyłem w drogę")
+        else:
+            print("Ruszyłam w drogę")
+
 
 # TypeError: Human.__init__() missing 2 required positional arguments: 'imie' and 'wiek'
 # cz1 = Human()
@@ -30,3 +36,13 @@ print(cz1.plec)
 # m
 cz1.powitanie()
 # Nazywam się: Radek
+
+cz2 = Human("Anna", 65)
+print(cz2.imie)  # Anna
+print(cz2.wiek)  # 65
+print(cz2.plec)  # k
+
+cz2.powitanie()  # Nazywam się: Anna
+
+cz1.ruszaj()  # Ruszyłem w drogę
+cz2.ruszaj()  # Ruszyłam w drogę
