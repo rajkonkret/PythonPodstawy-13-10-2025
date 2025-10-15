38aa9c01c6c4b47cf938b97325a9c1c464dff3cb
rajkonkret <rajkonkret660@gmail.com>
2025-10-15 09:44:56 +0200

    [2 6] add fun5.py

--- day3/fun5.py
+++ day3/fun5.py
@@ -5,3 +5,28 @@ def connect(**opcje):  # ** dowolna ilośc argumentów nazwanych (kwargs)
 connect()  # {}
 connect(a=7)  # {'a': 7}
 connect(a=7, name="Radek")  # {'a': 7, 'name': 'Radek'}
+
+
+def all_args(*args, **kwargs):
+    print(f"{args=}")
+    print(f"{kwargs=}")
+
+
+all_args()
+# args=()
+# kwargs={}
+
+all_args(1, 2, 3)
+# args=(1, 2, 3)
+# kwargs={}
+
+all_args(b=1, c=2, x=3)
+args = ()
+kwargs = {'b': 1, 'c': 2, 'x': 3}
+
+all_args(1, 2, name="radek")
+# args=(1, 2)
+# kwargs={'name': 'radek'}
+
+# SyntaxError: positional argument follows keyword argument
+# all_args(name="Radek", 1)
