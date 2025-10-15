b2d6ab36dc75fd0878bb35de078a35dd5e280414
rajkonkret <rajkonkret660@gmail.com>
2025-10-15 12:38:20 +0200

    [2 15] add kl3.py

---
+++ day3/kl3.py
@@ -0,0 +1,66 @@
+from abc import ABC, abstractmethod
+
+
+class Ptak(ABC):
+    """
+    Klasa Ptak w pythonie
+    """
+
+    def __init__(self, gatunek, szybkosc):
+        """
+        Metoda inicjalizująca
+        :param gatunek:
+        :param szybkosc:
+        """
+        self.gatunek = gatunek
+        self.szybkosc = szybkosc
+
+    def latam(self):
+        print("Tu", self.gatunek, "Lecę z szybkością", self.szybkosc)
+
+    @abstractmethod  # metoda abstarkcyjna
+    def wydaj_odglos(self):
+        pass
+
+
+class Kura(Ptak):
+    """
+    Klasa dziedziczy po klasie Ptak
+    """
+
+    def __init__(self, gatunek):
+        super().__init__(gatunek, 0)  # obowiązkowo musimy wywołać konstruktor z klasy wyższej
+
+    def latam(self):
+        print("Tu", self.gatunek, 'Ja nie latam.')
+
+    def wydaj_odglos(self):
+        print("Ko ko ko ko")
+
+
+class Orzel(Ptak):
+    """
+    Dziedziczy po Ptak
+    """
+
+    def wydaj_odglos(self):
+        print("kier kir kier")
+
+
+# nie mozna stworzyc obiektów klasy Ptak
+# TypeError: Can't instantiate abstract class Ptak without an implementation for abstract method 'wydaj_odglos'
+# or1 = Ptak("Orzeł", 50)
+# or1.latam()  # Tu Orzeł Lecę z szybkością 50
+#
+# kur1 = Ptak("Kura", 0)
+# kur1.latam()  # Tu Kura Lecę z szybkością 0
+
+kur2 = Kura("Kura 2")
+kur2.latam()  # Tu Kura 2 Ja nie latam.
+kur2.wydaj_odglos()  # Ko ko ko ko
+
+or2 = Orzel("Orzel Bielik", 55)
+or2.latam()
+or2.wydaj_odglos()
+# Tu Orzel Bielik Lecę z szybkością 55
+# kier kir kier
