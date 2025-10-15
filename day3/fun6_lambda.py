28569bd5d858d21bc73d280c456891b1ab6ace58
rajkonkret <rajkonkret660@gmail.com>
2025-10-15 10:27:15 +0200

    [2 78] update fun6_lambda.py.py

--- day3/fun6_lambda.py
+++ day3/fun6_lambda.py
@@ -15,3 +15,40 @@ print(odejmij2(10, 7))  # 3
 wiek = lambda x: "dziecko" if x < 10 else ("nastolatek" if x < 18 else "dorosły")
 print(wiek(10))  # nastolatek
 print(wiek(18))  # dorosły
+
+# mapowanie danych
+lista = [1, 2, 3, 4, 35, 55, 60, 100, 500]
+l1 = []
+for i in lista:
+    l1.append(i * 2)
+print(l1)  # [2, 4, 6, 8, 70, 110, 120, 200, 1000]
+
+print([i * 2 for i in lista])
+
+
+# [2, 4, 6, 8, 70, 110, 120, 200, 1000]
+
+def zmien(x):
+    return x * 2
+
+
+l2 = []
+for i in lista:
+    l2.append(zmien(i))
+print(l2)  # [2, 4, 6, 8, 70, 110, 120, 200, 1000]
+
+# map() - dla wszystkich eleemntów kolekcji wykonuje zadaną funkcję
+# funkcja wyzszego rzędu - przyjmuje inną funkcje jako argument
+
+# podajemy adres funkcji
+print(f"Zastosowanie map(): {list(map(zmien, lista))}")
+# Zastosowanie map(): [2, 4, 6, 8, 70, 110, 120, 200, 1000]
+
+# funkcja anonimowa - nie jest przypisana do jakiejkolwiek zmiennej
+# deklaracja w miejscu użycia
+print(f"Zastosowanie map(): {list(map(lambda x: x * 2, lista))}")
+# Zastosowanie map(): [2, 4, 6, 8, 70, 110, 120, 200, 1000]
+print(f"Zastosowanie map(): {list(map(lambda x: x * 3, lista))}")
+# Zastosowanie map(): [3, 6, 9, 12, 105, 165, 180, 300, 1500]
+print(f"Zastosowanie map(): {list(map(lambda x: x * 4, lista))}")
+# Zastosowanie map(): [4, 8, 12, 16, 140, 220, 240, 400, 2000]
